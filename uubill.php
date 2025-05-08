cd29255e44012b9c05fb3dbd05496d6462fd8c52
Saafmaker32 <saafmakerzz@gmail.com>
2025-05-08 18:58:48 +0200

    Add files via upload

--- uubill.php
+++ uubill.php
@@ -1,7 +1,6 @@
 <?php
 require_once('db_connect.php');
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $conn = // Database connection via db_connect.php
     if ($conn->connect_error) {
         die("Connectie mislukt: " . $conn->connect_error);
     }
